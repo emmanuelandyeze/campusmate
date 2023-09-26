df828bca09c4429eda192533c8185222aee131b1
emmanuelandyeze <emmanuel.andyeze@gmail.com>
2023-09-26 22:15:18 +0100

    first commit

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -6,8 +6,8 @@ function Landing() {
   return (
 		<section className=" text-black py-24 px-4 sm:px-3">
 			<div className="max-w-5xl mx-auto text-center">
-				<h1 className="text-4xl md:text-6xl font-bold mb-4 leading-10 md:leading-14 sm:leading-relaxed">
-					Your Companion. Simplify Your Student Life.
+				<h1 className="text-4xl md:text-6xl font-bold mb-4 leading-10 md:leading-snug sm:leading-relaxed">
+					Simplify Your Student Life <br /> With CampusMate.
 				</h1>
 				<p className="text-xl sm:text-2xl mb-8 w-full md:w-3/4 mx-auto leading-7 md:leading-relaxed">
 					Effortless Scheduling, Task Management,
